0ed5b2e993fe7c7c9999fb08f18f7b68a5b2a2ea
Darvillien37 <36336567+Darvillien37@users.noreply.github.com>
2020-09-29 13:43:35 +0100

    comments + renaming bot variable

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from dotenv import load_dotenv
 from Forebot.bot import Bot
 import logging
 
-# Load the enviroment variables
+# Load the environment variables
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-fbot = Bot(os.getenv("PREFIX"), TOKEN)
+# Initialise the bot
+fBot = Bot(os.getenv("PREFIX"), TOKEN)
 
-logger = fbot.get_logger()
+# Create the logger for the bot
+logger = fBot.get_logger()
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename="log.log", encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: \
     %(message)s'))
 logger.addHandler(handler)
-fbot.run()
+
+fBot.run()
